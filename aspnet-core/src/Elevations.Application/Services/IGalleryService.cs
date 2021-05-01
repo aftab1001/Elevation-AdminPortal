99da1e0172cf6c1d223722d320a497d3c527cecb
aftab1001 <aftab.rehman@outlook.com>
2021-05-01 21:03:12 +0500

    structure changes
    structure changes

--- aspnet-core/src/Elevations.Application/Services/IGalleryService.cs
+++ aspnet-core/src/Elevations.Application/Services/IGalleryService.cs
@@ -1,6 +1,15 @@
-﻿namespace Elevations.Services
+﻿using System.Threading.Tasks;
+using Abp.Application.Services;
+using Abp.Application.Services.Dto;
+using Elevations.EntityFrameworkCore.HotelDto;
+using Elevations.Roles.Dto;
+using Elevations.Services.Dto;
+
+namespace Elevations.Services
 {
-    public interface IGalleryService
+    public interface IGalleryService: IAsyncCrudAppService<GalleryDto, int, PagedResultRequestDto, Gallery,
+        GalleryDto>
     {
+        Task<ListResultDto<GalleryDto>> GetAllGalleryImages(PagedResultRequestDto input);
     }
 }
